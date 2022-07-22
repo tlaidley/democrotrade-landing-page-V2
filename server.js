6d9d9baf48e0f4c45ba0174b36f84a1d7ec6706f
tlaidley <94332530+tlaidley@users.noreply.github.com>
2022-07-22 00:05:25 -0500

    Set up default 404

--- server.js
+++ server.js
@@ -27,4 +27,9 @@ app.get("/", function (req, res, next) {
     })
     res.sendFile(__dirname + "index.html")
 })
+
+app.use((req, res, next) => {
+    res.status(404).send('Sry Page not found')
+})
+
 app.listen(3000, () => console.log('Server has started'))
