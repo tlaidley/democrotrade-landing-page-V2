682537f065aa5ce220322758eb87ddf6aaed9683
tlaidley <94332530+tlaidley@users.noreply.github.com>
2022-07-22 23:09:30 -0500

    Update script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,10 +4,18 @@ $(document).ready(function () {
         let waitListName = $("#waitListName").val();
         let waitListEmail = $("#waitListEmail").val();
         $.ajax({
-            url: "/subscribers",
+            url: "/api/subscribers",
             method: "POST",
             contentType: "application/json",
             data: JSON.stringify({ waitListName: waitListName, waitListEmail: waitListEmail }),
+            error: function (error) {
+                $("#JoinFormHeader").html(`Welcome to the Waiting List: ${error.message}`);
+                // $('#waitListForm').trigger("reset");
+                // $("#JoinForm").hide();
+                // $("#JoinResults").show();
+                console.log(error);
+                console.log('Error');
+            },
             success: function (res) {
                 $("#JoinFormHeader").html(`Welcome to the Waiting List: ${res.message}`);
                 $('#waitListForm').trigger("reset");
